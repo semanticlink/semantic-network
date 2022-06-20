5781f24c643bfcccd5a1cb736405c3cf887542a2
Ignacio De Maio <ignaciodemaio+formus@gmail.com>
2022-06-20 17:17:10 +1200

    Add tests to demonstrate `isTracked` current behaviour.
    - It theory it should not check whether the resource is a collection or not.

--- src/__tests__/trackedRepresentationUtil.ts
+++ src/__tests__/trackedRepresentationUtil.ts
@@ -1,17 +1,26 @@
-import { TrackedRepresentationUtil } from '../utils/trackedRepresentationUtil';
-import { CollectionRepresentation, LinkedRepresentation } from 'semantic-link';
-import { Status } from '../representation/status';
-import { SparseRepresentationFactory } from '../representation/sparseRepresentationFactory';
-import { Tracked } from '../types/types';
-import { instanceOfCollection } from '../utils/instanceOf/instanceOfCollection';
-import { instanceOfTrackedRepresentation } from '../utils/instanceOf/instanceOfTrackedRepresentation';
+import {TrackedRepresentationUtil} from '../utils/trackedRepresentationUtil';
+import {CollectionRepresentation, LinkedRepresentation} from 'semantic-link';
+import {Status} from '../representation/status';
+import {SparseRepresentationFactory} from '../representation/sparseRepresentationFactory';
+import {Tracked} from '../types/types';
+import {instanceOfCollection} from '../utils/instanceOf/instanceOfCollection';
+import {instanceOfTrackedRepresentation} from '../utils/instanceOf/instanceOfTrackedRepresentation';
+import {SingletonMerger} from '../../lib';
+
+type T = Tracked<LinkedRepresentation> & { me: LinkedRepresentation } & { users: CollectionRepresentation };
+const makeRoot = () => SparseRepresentationFactory.make({uri: 'https://example.com'}) as T;
+const makeMe = () => SparseRepresentationFactory.make(
+    {uri: 'https://example.com/me'}) as Tracked<LinkedRepresentation>;
+const makeUsersCollection = () => SparseRepresentationFactory.make(
+    {uri: 'https://example.com/users', sparseType: 'collection'}) as Tracked<LinkedRepresentation>;
+
 
 describe('Tracked Representation Utils', () => {
 
     describe('location only', function() {
         type T = Tracked<LinkedRepresentation> & { test: string };
         type K = keyof T;
-        const resource = SparseRepresentationFactory.make({ uri: 'https://example.com/1' }) as T;
+        const resource = SparseRepresentationFactory.make({uri: 'https://example.com/1'}) as T;
 
         it('getState, should be defined', () => {
             expect(resource).not.toBeNull();
@@ -40,7 +49,7 @@ describe('Tracked Representation Utils', () => {
         });
 
         it('should be a collection', () => {
-            const object: CollectionRepresentation = { links: [], items: [] };
+            const object: CollectionRepresentation = {links: [], items: []};
             expect(instanceOfCollection(object)).toBeTruthy();
         });
     });
@@ -48,10 +57,66 @@ describe('Tracked Representation Utils', () => {
     describe('add', () => {
         it('added item is tracked', () => {
             type T = Tracked<LinkedRepresentation> & { me: LinkedRepresentation };
-            const resource = SparseRepresentationFactory.make({ uri: 'https://example.com' }) as T;
-            const toAdd = SparseRepresentationFactory.make({ uri: 'https://example.com/me' }) as Tracked<LinkedRepresentation>;
+            const resource = SparseRepresentationFactory.make({uri: 'https://example.com'}) as T;
+            const toAdd = SparseRepresentationFactory.make({uri: 'https://example.com/me'}) as Tracked<LinkedRepresentation>;
             TrackedRepresentationUtil.add(resource, 'me', toAdd);
             expect(TrackedRepresentationUtil.isTracked(resource, 'me')).toBeTruthy();
+        });
+    });
+
+    describe('#isTracked', () => {
+        test.each([
+            //
+            // Expected to be tracked
+            //
+            ['singleton resource, and singleton tracked child', makeRoot(), 'me', makeMe(), true],
+            ['singleton resource, and collection tracked child', makeRoot(), 'users', makeUsersCollection(), true],
+            ['singleton resource, and not tracked singleton child', makeRoot(), 'me', {}, true],
+            ['singleton resource, and not tracked collection child', makeRoot(), 'users', { links: [], items: [] }, true],
+            ['singleton resource, and undefined child', makeRoot(), 'me', undefined, false],
+
+            ['collection resource, and collection tracked child', makeUsersCollection(), 'colleagues', makeUsersCollection(), true],
+            ['collection resource, and singleton tracked child', makeUsersCollection(), 'aSingletonThatMakesSense?', makeMe(), true],
+            ['collection resource, and not tracked singleton child', makeUsersCollection(), 'me', {}, true],
+            ['collection resource, and not tracked collection child', makeUsersCollection(), 'users', { links: [], items: [] }, true],
+
+            ['collection resource, and undefined child', makeUsersCollection(), 'colleagues', undefined, false],
+        ])('when conditionally tracking: %s', (
+            title,
+            resource: Tracked<LinkedRepresentation | CollectionRepresentation>,
+            propertyToAdd: string,
+            child: any,
+            expectedTracked: boolean) => {
+            if (child) {
+                TrackedRepresentationUtil.add(resource, propertyToAdd, child);
+            }
+
+            expect(TrackedRepresentationUtil.isTracked(resource, propertyToAdd)).toBe(expectedTracked);
+        });
+
+        test.each([
+            ['singleton resource, and singleton tracked child', makeRoot(), 'me', makeMe(), false],
+            ['singleton resource, and collection tracked child', makeRoot(), 'users', makeUsersCollection(), false],
+            ['singleton resource, and not tracked singleton child', makeRoot(), 'me', {}, false],
+            ['singleton resource, and not tracked collection child', makeRoot(), 'users', { links: [], items: [] }, false],
+            ['singleton resource, and undefined child', makeRoot(), 'me', undefined, false],
+
+            ['collection resource, and singleton tracked child', makeUsersCollection(), 'aSingletonThatMakesSense?', makeMe(), false],
+            ['collection resource, and collection tracked child', makeUsersCollection(), 'colleagues', makeUsersCollection(), false],
+            ['collection resource, and not tracked singleton child', makeUsersCollection(), 'me', {}, false],
+            ['collection resource, and not tracked collection child', makeUsersCollection(), 'users', { links: [], items: [] }, false],
+            ['collection resource, and undefined child', makeUsersCollection(), 'colleagues', undefined, false],
+        ])('is not tracked when not tracked correctly: %s', (
+            title,
+            resource: Tracked<LinkedRepresentation | CollectionRepresentation>,
+            propertyToAdd: string,
+            child: any,
+            expectedTracked: boolean) => {
+            if (child) {
+                SingletonMerger.add(resource, propertyToAdd, child);
+            }
+
+            expect(TrackedRepresentationUtil.isTracked(resource, propertyToAdd)).toBe(expectedTracked);
         });
     });
 
@@ -59,12 +124,12 @@ describe('Tracked Representation Utils', () => {
 
         it('true', () => {
             type T = Tracked<LinkedRepresentation>;
-            const resource = SparseRepresentationFactory.make({ uri: 'https://example.com/1' }) as T;
+            const resource = SparseRepresentationFactory.make({uri: 'https://example.com/1'}) as T;
             expect(instanceOfTrackedRepresentation(resource)).toBeTruthy();
         });
 
         it('false', () => {
-            const resource = { links: [] };
+            const resource = {links: []};
             expect(instanceOfTrackedRepresentation(resource)).toBeFalsy();
         });
     });

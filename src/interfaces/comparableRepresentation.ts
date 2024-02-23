779be49010d0b224448a4267af6af4698e9ebf8e
todd <todd+github@goneopen.com>
2024-02-23 19:53:50 +1300

    feat: add feed item mappings of eTag to collection items that previous exists

--- src/interfaces/comparableRepresentation.ts
+++ src/interfaces/comparableRepresentation.ts
@@ -1,4 +1,3 @@
 import { LinkedRepresentation } from 'semantic-link';
-import { Tracked } from '../types/types';
 
 export type ComparableRepresentation = LinkedRepresentation & { name?: string, id?: string }
